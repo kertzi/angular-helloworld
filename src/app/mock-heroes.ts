6762c13c9ccf70dae2a7208ed894d9eb00530387
Mikko K <kertzi@gmail.com>
2018-10-08 13:35:39 +0300

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Long John' },
+    { id: 12, name: 'Narkki' },
+    { id: 13, name: 'Magneta' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Mockka' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }    
+];
